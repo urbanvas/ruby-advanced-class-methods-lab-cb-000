b9f52f031a1be844438c0c9e6ce0133517faaec0
Neil Seejoor <nvseejoor@gmail.com>
2018-02-20 03:30:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,29 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name=(title)
+    song
+  end
+
+  def self.create_by_name(title)
+    song = self.new_by_name(title)
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(string)
+    self.all.detect {|i| i.name == string}
+  end
+
+  def self.find_or_create_by_name(string)
+    self.find_by_name(string) ? self.find_by_name(title) : self.create_by_name(title)
+  end
 end
